82d944383dc5004709ccde4a3252d5543db86678
Richard Wong <ricwtk@gmail.com>
2014-01-22 17:03:03 +0000

    rework ui to include hamburger/slide-out drawer

--- background.js
+++ background.js
@@ -2,10 +2,10 @@ chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
 	  bounds: {
 		  width: 300, 
-		  height: 70
+		  height: 500
 	  },
 	  minWidth: 300,
-	  minHeight: 70,
+	  minHeight: 500,
 	  maxWidth: 300
   });
 });
