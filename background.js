6d20b25cfa99c0b134e7c676472d2dc2b6e64b7a
Richard Wong <ricwtk@gmail.com>
2014-01-22 15:29:36 +0000

    finalise main ui

--- background.js
+++ background.js
@@ -1,8 +1,11 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
 	  bounds: {
-		  width: 800, 
-		  height: 500
-	  }
+		  width: 300, 
+		  height: 70
+	  },
+	  minWidth: 300,
+	  minHeight: 70,
+	  maxWidth: 300
   });
 });
