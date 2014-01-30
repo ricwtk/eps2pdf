259a50bb623a53f745246066388cd4561a173756
unknown <richard.wong@eng.ox.ac.uk>
2014-01-30 11:58:58 +0000

    add div for menu

--- action.js
+++ action.js
@@ -1,11 +1,11 @@
 function openMenu() {
-	document.getElementById('maindiv').style.left='270px';
+	document.getElementById('maindiv').style.left='0px';
 	document.getElementById('menu').removeEventListener('click', openMenu);
 	document.getElementById('menu').addEventListener('click', closeMenu);
 };
 
 function closeMenu() {
-	document.getElementById('maindiv').style.left='0px';
+	document.getElementById('maindiv').style.left='-270px';
 	document.getElementById('menu').removeEventListener('click', closeMenu);
 	document.getElementById('menu').addEventListener('click', openMenu);
 }
